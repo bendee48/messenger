92d1f7a608e739fcc9fdae585ed6980f0c5b397f
Ben Daniels <benbanj@hotmail.com>
2021-02-23 16:48:03 +0000

    Saving messages to db

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
   def create
+    @message = current_user.messages.build(message_params)
+    @message.save
+    ActionCable.server.broadcast('message', @message.as_json(include: :user))
+    redirect_to root_path
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
